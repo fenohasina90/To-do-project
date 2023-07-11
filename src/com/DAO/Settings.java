a15214d61126a5c1b0349f94bd5e45b9f787b436
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-07-11 14:53:02 +0300

    Feat: Settings file

---
+++ src/com/DAO/Settings.java
@@ -0,0 +1,7 @@
+package com.DAO;
+
+public class Settings {
+    public static final String DATABASE_NAME = "database_name";
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "your_password";
+}
