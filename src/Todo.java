96f16c9b50d45708ac1b2f3f0ed2418fd0cc052f
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-07-13 19:45:37 +0300

    feat: add Todo.java class

---
+++ src/Todo.java
@@ -0,0 +1,37 @@
+public class Todo {
+    private String title;
+    private String description;
+    private int priority;
+
+    public Todo(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    // Getters et Setters pour les attributs
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
