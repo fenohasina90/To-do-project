14e150d4ed022e422760d0653f4bd3484c5590cc
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-07-14 09:08:56 +0300

    Fix: update

--- src/Todo.java
+++ src/Todo.java
@@ -1,13 +1,21 @@
+import java.sql.Timestamp;
+
 public class Todo {
+    private int id;
     private String title;
     private String description;
+    private Timestamp deadline;
     private int priority;
+    private boolean done;
 
     // Constructeur
-    public Todo(String title, String description, int priority) {
+    public Todo(int id, String title, String description,Timestamp deadline, int priority,boolean done) {
+        this.id = id;
         this.title = title;
         this.description = description;
+        this.deadline = deadline;
         this.priority = priority;
+        this.done = done;
     }
 
     // Getters et Setters
@@ -33,5 +41,24 @@ public class Todo {
 
     public void setPriority(int priority) {
         this.priority = priority;
+    }
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public Timestamp getDeadline() {return deadline;}
+
+    public void setDeadline(Timestamp deadline) {this.deadline = deadline;}
+
+    public boolean isDone() {return done;}
+
+    public void setDone(boolean done) {this.done = done;}
+
+    @Override
+    public String toString() {
+        return
+            "id=" + id + ", title='" + title +", description='" + description +", deadline=" + deadline +
+                ", priority=" + priority + ", done=" + done ;
     }
 }
